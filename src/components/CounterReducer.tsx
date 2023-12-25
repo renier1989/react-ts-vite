dfc85b86ea084bbd1dac5908a65a612d41bd21a4
Renier Vargas <renier1989@gmail.com>
2023-12-25 13:40:44 -0500

    desde desktop omitir estos cambios

---
+++ src/components/CounterReducer.tsx
@@ -0,0 +1,23 @@
+const initialState = {
+    contador:0,
+}
+
+const contadorReducer = (state,action)=>{
+    switch (action) {
+        case 'incrementar':
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+
+export const CounterReducer = () => {
+    return (
+        <>
+            <div>Counter Reducer : <span>{10}</span> </div>
+        </>
+    )
+}
